d609002978addc6adc76cd4b01a32f9523be3ca7
Yifei-Li <li.yifei9930@gmail.com>
2016-04-23 11:48:27 -0400

    routine commit

--- javascript/resize.js
+++ javascript/resize.js
@@ -1,10 +1,10 @@
-$(function(){
-    function setWidth (middle, d1, d2)  {
-        
-        
-    }
-    
-    var arr = [];
-    
-    
-});
+//forces height and width of project container to be equal to how the images render
+
+/*$(function(){
+    $("#projectImage").load(function()   {
+        var height = $(this).height();
+        var width = $(this).width();
+        $(".project").width(width);
+        $(".project").height(height);
+    });
+});*/
